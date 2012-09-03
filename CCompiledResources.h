5619cd01b2a65c8eece5abb44e35c8a329bd8ced
Sergey Sergeev <sergey.sergeev.oneclick@gmail.com>
2012-09-04 00:22:12 +0300

    added compiled script

---
+++ CCompiledResources.h
@@ -0,0 +1,19 @@
+//
+//  CCompiledResources.h
+//  iGaia
+//
+//  Created by Sergey Sergeev on 9/3/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+/*{%LOAD_TEMPLATE_H%}
+ + (UIImage*)SANITISED_FILENAME_image;
+ {%LOAD_TEMPLATE_H END%}*/
+
+/*{%IMAGELOADERS_H START%}*/
+/*{%IMAGELOADERS_H END%}*/
+
+
+
---
+++ CCompiledResources.m
@@ -0,0 +1,12 @@
+#import "CCompiledResources.h"
+
+/*{%LOAD_TEMPLATE_M%}
++ (UIImage*)SANITISED_FILENAME_image
+ {
+    return [UIImage imageNamed:@"ORIGINAL_FILENAME"];
+ }
+ 
+{%LOAD_TEMPLATE_M END%}*/
+
+/*{%IMAGELOADERS_M START%}*/
+/*{%IMAGELOADERS_M END%}*/
