d8c4ddd5d8d650ca8da5fcaab4d4eda257fc18e0
Sergey Sergeev <sergey.sergeev@yes-3.local>
2013-02-06 18:42:56 +0200

    fixed landscape orientation

--- Classes/MainAppDelegate.mm
+++ Classes/MainAppDelegate.mm
@@ -13,7 +13,7 @@
     m_pNavigationViewController = [[UINavigationController alloc] initWithRootViewController:vc];
     [m_pNavigationViewController setNavigationBarHidden:YES animated:NO];
     m_pWindow.backgroundColor = [UIColor blackColor];
-    [m_pWindow addSubview:m_pNavigationViewController.view];
+    [m_pWindow setRootViewController:m_pNavigationViewController];
     [m_pWindow makeKeyAndVisible];
     
 }  
