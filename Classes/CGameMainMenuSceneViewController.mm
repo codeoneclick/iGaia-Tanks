3c4be8d079c3eeebad7f88523bd8b4c0cb800673
Sergey Sergeev <sergey.sergeev@sergeysergeev.local>
2013-03-13 19:19:06 +0200

    added move controller and character controller

--- Classes/CGameMainMenuSceneViewController.mm
+++ Classes/CGameMainMenuSceneViewController.mm
@@ -11,11 +11,13 @@
 #include "CGLWindow_iOS.h"
 #include "CGameMainMenuScene.h"
 #include "CMainLoop_iOS.h"
+#include "CMoveControllerView_iOS.h"
 
 @interface CGameMainMenuSceneViewController ()
 
 @property(weak, nonatomic) IBOutlet CGLWindow_iOS *m_glView;
 @property(unsafe_unretained, nonatomic) CGameMainMenuScene* m_scene;
+@property (strong, nonatomic) IBOutlet CMoveControllerView_iOS *m_moveController;
 
 @end
 
@@ -38,6 +40,7 @@
     CRoot_iOS* root = new CRoot_iOS((__bridge void*)_m_glView);
     self.m_scene = new CGameMainMenuScene();
     self.m_scene->Load(root);
+    [self.m_moveController AddEventListener:self.m_scene->Get_CharacterController()];
 }
 
 - (void)didReceiveMemoryWarning
@@ -48,6 +51,7 @@
 - (void)viewDidUnload
 {
     [self setM_glView:nil];
+    [self setM_moveController:nil];
     [super viewDidUnload];
 }
 @end
